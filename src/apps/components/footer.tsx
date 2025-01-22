b1f91ec0249c570efbd294f6ac2c7674f28ebe5a
Sagar Gnawali <sagargnawali2@gmail.com>
2025-01-22 17:05:28 +0545

    feat: adjust the footer with days

--- src/apps/components/footer.tsx
+++ src/apps/components/footer.tsx
@@ -1,19 +1,14 @@
-export const getDayName = (d: number): string => {
-  const day = {
-    0: "Sunday",
-    1: "Monday",
-    2: "Tuesday",
-    3: "Wednesday",
-    4: "Thursday",
-    5: "Friday",
-    6: "Saturday",
-  };
-  //@ts-ignore
-  return day[d];
-};
+import { useLocation } from "react-router-dom";
+import { getDayName } from "../../utils";
 export default function Footer() {
+  const location = useLocation();
+  const isHomePath = location.pathname === "/";
   return (
-    <footer className="flex justify-center gap-1 text-cente bg-gray-800 absolute text-white text-center py-4 mt-auto bottom-0 w-full">
+    <footer
+      className={`flex justify-center gap-1 text-cente bg-gray-800  ${
+        isHomePath ? "absolute" : "static"
+      } bottom-0 text-white text-center py-4 mt-auto w-full`}
+    >
       <p> Rights reserved © {new Date().getFullYear()}. </p>
       <p className="bg-gradient-to-r from-blue-300 via-orange-300 to-red-300 inline-block text-transparent bg-clip-text">
         {""}Have a good {getDayName(new Date().getDay())}!

fb2c50bdaa7b89a7210d01500ef36334c5b477a5
Sagar Gnawali <sagargnawali2@gmail.com>
2025-02-16 15:11:41 +0545

    fix footer issue

--- src/apps/components/container.tsx
+++ src/apps/components/container.tsx
@@ -4,7 +4,9 @@ type ContainerProps = {
 };
 export default function Container({ children, className }: ContainerProps) {
   return (
-    <div className={`p-[10px] m-[50px] mx-[20px] md:m-[40px] md:mx-[200px] ${className}}`}>
+    <div
+      className={`p-[10px] m-[50px] mx-[20px] md:m-[40px] md:mx-[200px] ${className}}`}
+    >
       {children}
     </div>
   );
--- src/apps/components/footer.tsx
+++ src/apps/components/footer.tsx
@@ -1,13 +1,8 @@
-import { useLocation } from "react-router-dom";
 import { getDayName } from "../../utils";
 export default function Footer() {
-  const location = useLocation();
-  const isHomePath = location.pathname === "/";
   return (
     <footer
-      className={`flex justify-center gap-1 text-cente bg-gray-800  ${
-        isHomePath ? "absolute" : "static"
-      } bottom-0 text-white text-center py-4 mt-auto w-full`}
+      className={`flex justify-center gap-1 text-cente bg-gray-800 bottom-0 text-white text-center py-4 mt-auto w-full`}
     >
       <p> Rights reserved © {new Date().getFullYear()}. </p>
       <p className="bg-gradient-to-r from-blue-300 via-orange-300 to-red-300 inline-block text-transparent bg-clip-text">

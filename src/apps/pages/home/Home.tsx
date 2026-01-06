27a307310af38a2ba2727cf6fae0ca0c19d67b5d
Sagar Gnawali <sagargnawali2@gmail.com>
2026-01-06 22:37:33 +0000

    update home section

--- src/apps/pages/home/Home.tsx
+++ src/apps/pages/home/Home.tsx
@@ -29,7 +29,7 @@ export default function Home() {
               {" "}
               Software
             </span>{" "}
-            developer, passionate
+            Engineer, passionate
             <br />
             <span>
               about{" "}
